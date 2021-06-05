3326738dbf16ba9def774e8adae30b954f1de042
thehunckerma <thehunckerma@hotmail.com>
2021-06-06 00:53:59 +0100

    Add fresh project + basic auth

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,41 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using System.Threading.Tasks;
+using dot_net.Models;
+using dot_net.Services;
+
+namespace dot_net.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("[controller]")]
+    [Consumes("application/json")]
+    public class UsersController : ControllerBase
+    {
+        private IUserService _userService;
+
+        public UsersController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [AllowAnonymous]
+        [HttpPost("authenticate")]
+        public async Task<IActionResult> Authenticate([FromBody] AuthenticateModel model)
+        {
+            var user = await _userService.Authenticate(model.Username, model.Password);
+
+            if (user == null)
+                return BadRequest(new { message = "Username or password is incorrect" });
+
+            return Ok(user);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var users = await _userService.GetAll();
+            return Ok(users);
+        }
+    }
+}
