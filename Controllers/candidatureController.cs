bdd1731283d5c47768cc901b1592ea9921d9efe1
AhmedELkhdadi <ahmedelkhdadi@gmail.com>
2021-06-20 21:48:24 +0100

    add justificative upload route

--- Controllers/candidatureController.cs
+++ Controllers/candidatureController.cs
@@ -7,6 +7,7 @@ using dot_net.Services;
 using System.Text.Json;
 using Microsoft.AspNetCore.Hosting;
 using System.IO;
+using System;
 namespace dot_net.Controllers
 {
     [Authorize]
@@ -24,14 +25,33 @@ namespace dot_net.Controllers
         [HttpPost("new")]
         public IActionResult addcandidature([FromBody] CandidatureModel model)
         {
+            // temporary
             string json = System.Text.Json.JsonSerializer.Serialize(model);
-            string filePath= Path.Combine(_hostEnvironment.WebRootPath, "candidatures/"+ model.firstname + model.lastname + ".json");
-            try{
-                System.IO.File.WriteAllText(filePath, json);
-                return Ok();
-            }catch{
-                return StatusCode(500);
-            }
+            return Ok();
+            // string filePath= Path.Combine(_hostEnvironment.WebRootPath, "candidatures/"+ model.firstname + model.lastname + ".json");
+            // try{
+            //     System.IO.File.WriteAllText(filePath, json);
+            //     return Ok();
+            // }catch{
+            //     return StatusCode(500);
+            // }
+        }
+        
+        [HttpPost("justificative")] 
+        [Consumes("multipart/form-data")]
+        public async Task<IActionResult> UploadJustificative() {
+            var postedFile = Request.Form.Files["justificative"];
+            if (postedFile != null){ 
+                string fileExtension = Path.GetExtension(postedFile.FileName);
+                Guid guid = Guid.NewGuid(); 
+                string fileName = guid + fileExtension; 
+                string filePath = Path.Combine(_hostEnvironment.WebRootPath, "justificatives/" + fileName);
+                using (var stream = new FileStream(filePath, FileMode.Create)){ 
+                    await postedFile.CopyToAsync(stream);
+                    return Ok(fileName);
+                }
+            }      
+            return StatusCode(400);
         }
     }
 }
